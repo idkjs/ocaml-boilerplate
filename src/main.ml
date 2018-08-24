bcc4c6c4d2954a27e14841eec57652016df7897e
Gyu Kang <hello@gyukang.me>
2018-08-25 05:29:57 +0900

    Implement skeleton code about parsing command line inputs

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,30 @@
-let main () = print_endline "Hello, World!"
+exception Unsupported_command
+
+type command = 
+  | HELP
+  | NEW of string
+
+let get_command (arguments: string list) = 
+  match arguments with
+  | ["help"] -> HELP
+  | ["new"; project_name] -> NEW project_name
+  | _ -> raise Unsupported_command
+
+let display_usage () = print_endline
+  "Usage:
+    ocaml-boilerplate help                  Display full usage
+    ocaml-boilerplate new <project_name>    Create a new project which is named <project_name>"
+
+(* TODO *)
+let create_project (project_name: string) = ()
+
+let run (command: command) =
+  match command with
+  | HELP -> display_usage ()
+  | NEW project_name -> create_project project_name
+
+let () =
+  try let command = get_command (List.tl (Array.to_list Sys.argv)) in
+  run command
+  with Unsupported_command -> display_usage()
+  
