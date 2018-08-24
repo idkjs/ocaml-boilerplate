f97754c476ddc8e3cffe39c116a3498a92e5bb44
Gyu Kang <hello@gyukang.me>
2018-08-25 08:10:27 +0900

    Implement command to create a new project using 'oasis'

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,5 @@
+open Unix
+
 exception Unsupported_command
 
 type command = 
@@ -15,8 +17,31 @@ let display_usage () = print_endline
     ocaml-boilerplate help                  Display full usage
     ocaml-boilerplate new <project_name>    Create a new project which is named <project_name>"
 
-(* TODO *)
-let create_project (project_name: string) = ()
+let create_project (project_name: string) =
+  Printf.printf "Creating a project '%s'...\n" project_name;
+  
+  Unix.mkdir project_name 0o755;
+  Sys.chdir project_name;
+
+  let quickstart_command = "oasis quickstart" in
+  Printf.printf "Running '%s'...\n%!" quickstart_command;
+  ignore (Sys.command quickstart_command);
+  print_endline "[OK] A file '_oasis' was created.\n";
+
+  let setup_command = "oasis setup" in
+  Printf.printf "Running '%s'...\n%!" setup_command;
+  ignore (Sys.command setup_command);
+  print_endline "[OK] A file 'setup.ml' was created.\n";
+
+  let configure_command = "ocaml setup.ml -configure" in 
+  Printf.printf "Running '%s'...\n%!" configure_command;
+  ignore (Sys.command configure_command);
+
+  let build_command = "ocaml setup.ml -build" in 
+  Printf.printf "Running '%s'...\n%!" build_command;
+  ignore (Sys.command build_command);
+
+  Printf.printf "\nA project '%s' was created successfully!\n%!" project_name
 
 let run (command: command) =
   match command with
