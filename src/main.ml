a6dfc75316f1311eced9d1a09c7a6a292c580861
Gyu Kang <hello@gyukang.me>
2018-08-25 23:13:13 +0900

    Do not run 'oasis setup' if an user decided to run 'oasis setup' during the process of 'oasis quickstart'

--- src/main.ml
+++ src/main.ml
@@ -51,13 +51,14 @@ let create_project (project_name: string) =
   let quickstart_command = "oasis quickstart" in
     print_message ("Running '" ^ quickstart_command ^ "'...");
     (match Unix.system quickstart_command with
-      | WEXITED 0 -> print_message "[OK] A file '_oasis' was created."
+      | WEXITED 0 -> print_message "[OK] A file '_oasis' was created.\n"
       | _ -> ignore (exit 0));  
 
-  let setup_command = "oasis setup" in
-    print_message ("Running '" ^ setup_command ^ "'...");
-    ignore (Sys.command setup_command);
-    print_message "[OK] A file 'setup.ml' was created.";
+  if not (Sys.file_exists "setup.ml") then
+    (let setup_command = "oasis setup" in
+      print_message ("Running '" ^ setup_command ^ "'...");
+      ignore (Sys.command setup_command);
+      print_message "[OK] A file 'setup.ml' was created.\n");
 
   let configure_command = "ocaml setup.ml -configure" in 
     print_message ("Running '" ^ configure_command ^ "'...");
